93c1d0cea69048f3cd6539ae54becbdb1017ab10
Shridatta Dixit <shridattadixit.cs25@bmsce.ac.in>
2026-08-31 13:59:08 +0530

    Init message display on boot

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,13 +2,27 @@ bits 16
 org 0x7c00
 
 start:
+
     mov ah, 0x0e
-    mov al, "A"
+
+    mov si, message
+
+print_loop:
+
+    mov al, [si]
+
+    cmp al, 0
+    je hang
+
     int 0x10
+
+    inc si
+    jmp print_loop
 
 hang:
     hlt
     jmp hang
 
+message db 'Arceon', 0
 times 510-($-$$) db 0
 dw 0xaa55
