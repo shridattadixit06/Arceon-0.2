fc1f7cce3296c4876e5e1f3dba092f68746faed6
Shridatta Dixit <shridattadixit.cs25@bmsce.ac.in>
2026-09-23 14:47:41 +0530

    cleared the default-qemu interface and added a message to the screen when the kernel starts.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,25 +1,17 @@
 void kernel_main()
 {
     volatile char *vga = (volatile char *)0xB8000;
+    volatile char *msg = "KERNEL has started!";
+    for (int i = 0; i < 80 * 25 * 2; i++)
+    {
+        vga[i] = 0;
+    }
 
-    vga[0] = 'K';
-    vga[1] = 0x10;
-
-    vga[2] = 'E';
-    vga[3] = 0x10;
-
-    vga[4] = 'R';
-    vga[5] = 0x10;
-
-    vga[6] = 'N';
-    vga[7] = 0x10;
-
-    vga[8] = 'E';
-    vga[9] = 0x10;
-
-    vga[10] = 'L';
-    vga[11] = 0x10;
-
+    for (int i = 0; i < 24; i++)
+    {
+        vga[i * 2] = msg[i];
+        vga[i * 2 + 1] = 0x07;
+    }
     while (1)
     {
     }
